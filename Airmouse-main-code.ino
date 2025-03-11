cdb8c4b88a908f5b94e0fb941e3a8edd503b8d8b
Gautamv2007 <gautam.rrv@gmail.com>
2025-03-11 18:09:37 +0530

    Added new Kalman Filtering and scroll up and scroll down button.

--- Airmouse-main-code.ino
+++ Airmouse-main-code.ino
@@ -1,58 +1,115 @@
-#include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <MadgwickAHRS.h>
 #include <BleMouse.h>
+#include <Adafruit_MPU6050.h>
+
+#define LEFTBUTTON 19
+#define RIGHTBUTTON 18
+#define UPBUTTON 21
+#define DOWNBUTTON 22
+#define SPEED 10
+#define DRAG_THRESHOLD 1000 // Time in milliseconds to trigger drag
 
 Adafruit_MPU6050 mpu;
-Madgwick filter;
-BleMouse bleMouse("GyroMouse", "ESP32", 100);
+BleMouse bleMouse;
 
-float roll, pitch, yaw;
+bool sleepMPU = true;
+long leftButtonPressedTime = 0;
+bool isDragging = false;
+
+// Kalman filter variables
+float estimatedX = 0, estimatedZ = 0;
+float errorCovX = 1, errorCovZ = 1;
+float processNoise = 0.1; // Adjust for smoothness
+float measurementNoise = 0.5; // Adjust for sensitivity
+
+// Kalman filter function
+float kalmanFilter(float measurement, float &estimate, float &errorCov) {
+  errorCov += processNoise;
+  float kalmanGain = errorCov / (errorCov + measurementNoise);
+  estimate = estimate + kalmanGain * (measurement - estimate);
+  errorCov = (1 - kalmanGain) * errorCov;
+  return estimate;
+}
 
 void setup() {
-    Serial.begin(115200);
-    Wire.begin();
-    
-    if (!mpu.begin()) {
-        Serial.println("MPU6050 connection failed!");
-        while (1);
-    }
-    Serial.println("MPU6050 connected!");
-    filter.begin(100);  // Set filter update rate to 100Hz
-    bleMouse.begin();
+  Serial.begin(115200);
+  pinMode(LEFTBUTTON, INPUT_PULLUP);
+  pinMode(RIGHTBUTTON, INPUT_PULLUP);
+  pinMode(UPBUTTON, INPUT_PULLUP);
+  pinMode(DOWNBUTTON, INPUT_PULLUP);
+  
+  bleMouse.begin();
+
+  delay(1000);
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) { delay(10); }
+  }
+  Serial.println("MPU6050 Found!");
+  mpu.enableSleep(sleepMPU);
 }
 
 void loop() {
-    if (bleMouse.isConnected()) {
-        sensors_event_t a, g, temp;
-        mpu.getEvent(&a, &g, &temp);
-        
-        // Convert gyro data to deg/sec
-        float gx_r = g.gyro.x * 180 / PI;
-        float gy_r = g.gyro.y * 180 / PI;
-        float gz_r = g.gyro.z * 180 / PI;
-        
-        // Convert accelerometer data to g
-        float ax_r = a.acceleration.x / 9.81;
-        float ay_r = a.acceleration.y / 9.81;
-        float az_r = a.acceleration.z / 9.81;
-        
-        // Update filter with new IMU data
-        filter.updateIMU(gx_r, gy_r, gz_r, ax_r, ay_r, az_r);
-        
-        // Get orientation
-        roll = filter.getRoll();
-        pitch = filter.getPitch();
-        yaw = filter.getYaw();
-        
-        // Convert orientation to mouse movement
-        int dx = map(yaw, -90, 90, -10, 10);  // Adjust sensitivity as needed
-        int dy = map(pitch, -90, 90, -10, 10);
-        
-        // Send mouse movement via BLE
-        bleMouse.move(dx, dy);
+  if (bleMouse.isConnected()) {
+    if (sleepMPU) {
+      delay(3000);
+      Serial.println("MPU6050 awakened!");
+      sleepMPU = false;
+      mpu.enableSleep(sleepMPU);
+      delay(500);
     }
-    
-    delay(10);  // Small delay for stability
+
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    float gyroX = g.gyro.x * -SPEED;
+    float gyroZ = g.gyro.z * -SPEED;
+
+    float filteredX = kalmanFilter(gyroX, estimatedX, errorCovX);
+    float filteredZ = kalmanFilter(gyroZ, estimatedZ, errorCovZ);
+
+    bleMouse.move(filteredZ, filteredX);
+
+    // Left Click / Drag Logic
+    if (!digitalRead(LEFTBUTTON)) {
+      if (leftButtonPressedTime == 0) {
+        leftButtonPressedTime = millis();
+      }
+      if (!isDragging && millis() - leftButtonPressedTime > DRAG_THRESHOLD) {
+        Serial.println("Dragging started");
+        bleMouse.press(MOUSE_LEFT);
+        isDragging = true;
+      }
+    } else {
+      if (isDragging) {
+        Serial.println("Dragging stopped");
+        bleMouse.release(MOUSE_LEFT);
+        isDragging = false;
+      } else if (leftButtonPressedTime > 0) {
+        Serial.println("Left click");
+        bleMouse.click(MOUSE_LEFT);
+      }
+      leftButtonPressedTime = 0;
+    }
+
+    // Right Click Logic
+    if (!digitalRead(RIGHTBUTTON)) {
+      Serial.println("Right click");
+      bleMouse.click(MOUSE_RIGHT);
+      delay(500);
+    }
+
+    // Scroll Up Logic
+    if (!digitalRead(UPBUTTON)) {
+      Serial.println("Scrolling up");
+      bleMouse.move(0, 0, 1); // Scroll up
+      delay(300); // Debounce delay
+    }
+
+    // Scroll Down Logic
+    if (!digitalRead(DOWNBUTTON)) {
+      Serial.println("Scrolling down");
+      bleMouse.move(0, 0, -1); // Scroll down
+      delay(300); // Debounce delay
+    }
+  }
 }
